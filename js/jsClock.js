354f5fe63560c43d7f387355f34efdd00ec1b32f
Elder <elder_louzada@outlook.com>
2019-05-27 12:28:08 -0300

    jsClock refresh bug correted

--- js/jsClock.js
+++ js/jsClock.js
@@ -1,34 +1,37 @@
-$(function (e) {
-    function getHours(date) {
-        let hour = ''
-        if (date.getHours() < 10)
-            hour = `0${date.getHours()}`
-        else if (date.getHours() <= 12)
-            hour = date.getHours()
-        else
-            hour = (date.getHours() - 12) < 10 ? `0${(date.getHours() - 12)}` : date.getHours() - 12
-        console.log(hour, 'here')
-        return hour
+function getHours(date) {
+    let hour = ''
+    if (date.getHours() < 10)
+        hour = `0${date.getHours()}`
+    else if (date.getHours() <= 12)
+        hour = date.getHours()
+    else
+        hour = (date.getHours() - 12) < 10 ? `0${(date.getHours() - 12)}` : date.getHours() - 12
+    console.log(hour, 'here')
+    return hour
+}
+function getMinutes(date) {
+    return date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+}
+function getDaytime(date) {
+    return date.getHours() < 12 ? 'AM' : 'PM'
+}
+function getMonthName(date) {
+    return Array.from(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jul', 'Jun', 'Ago', 'Sep', 'Oct', 'Nov', 'Dec'])[date.getMonth()]
+}
+function initClock(date) {
+    return {
+        hour: getHours(date),
+        minutes: getMinutes(date),
+        daytime: getDaytime(date),
+        month: getMonthName(date),
+        day: date.getDate(),
+        year: date.getFullYear()
     }
-    function getMinutes(date) {
-        return date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getHours()
-    }
-    function getDaytime(date) {
-        return date.getHours() < 12 ? 'AM' : 'PM'
-    }
-    function getMonthName(date) {
-        return Array.from(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jul', 'Jun', 'Ago', 'Sep', 'Oct', 'Nov', 'Dec'])[date.getMonth()]
-    }
-    const date = new Date
-    const clock = {}
-    clock.hour = getHours(date)
-    clock.minutes = getMinutes(date)
-    clock.daytime = getDaytime(date)
-    clock.month = getMonthName(date)
-    clock.day = date.getDate()
-    clock.year = date.getFullYear()
+}
 
+$(function (e) {
     setInterval(() => {
+        let clock = initClock(new Date)
         $('.hour-minute').text(`${clock.hour}:${clock.minutes}`)
         $('.daytime').text(`${clock.daytime}`)
         $('.date').text(`${clock.day} ${clock.month}, ${clock.year}`)
